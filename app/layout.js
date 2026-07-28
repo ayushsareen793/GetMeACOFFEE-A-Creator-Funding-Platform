ee19581a7f601b91a227389cf4844c2ec4b8b68d
Ayush <ayushsareen793@gmail.com>
2026-07-29 01:18:11 +0530

    favicon

--- app/layout.js
+++ app/layout.js
@@ -21,10 +21,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col  bg-[#0c0c0e] text-white">
        <SessionWrapper>
         <Navbar />
